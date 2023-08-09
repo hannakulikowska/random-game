d8c792902fc10ec40f3620a3eba0bdd699b04393
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2023-08-09 16:38:01 +0200

    feat: add burger-menu

--- library/index.js
+++ library/index.js
@@ -1,9 +1,43 @@
-console.log(`Привет!
-Если посчитаешь, что что-то не соответсвует требованиям, пожалуйста, сообще мне и я сразу исправлю.
-Поясню сразу насчет футера - там всё в соответствии с требованиями и макетом. По макету между годом, названием школы и именем одинаковое расстояние, название школы уже не центрируется в макете, как раньше при 1440 пикс. У меня сделано именно так как в макете - между флексовыми элеменами задано одинаковое расстояние при уменьшении экрана.
+// Greeting
 
-Самопроверка:
-Вёрстка соответствует макету. Ширина экрана 768px.
-Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки.
-Весь контент страницы при этом сохраняется: не обрезается и не удаляется.
-На ширине экрана 768рх реализовано адаптивное меню.`);
+console.log(`Самопроверка:
+1. Вёрстка соответствует макету. Ширина экрана 768px.
+2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется.
+3. На ширине экрана 768рх реализовано адаптивное меню.`);
+
+
+// Burger handler
+
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.nav');
+  const icon = document.querySelector('.profile-icon');
+  const menuCloseItem = document.querySelector('.nav-close');
+  const menuLinks = document.querySelectorAll('.nav-link');
+
+  const closeMenu = () => {
+    menu.classList.remove('nav-active');
+    icon.classList.remove('profile-icon-fixed');
+  };
+
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('nav-active');
+    icon.classList.add('profile-icon-fixed');
+  });
+
+  menuCloseItem.addEventListener('click', closeMenu);
+
+  if (window.innerWidth <= 1124) {
+    for (let i = 0; i < menuLinks.length; i++) {
+      menuLinks[i].addEventListener('click', closeMenu);
+      icon.classList.remove('profile-icon-fixed');
+    }
+  }
+
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!menu.contains(target) && !burgerItem.contains(target)) {
+      closeMenu();
+    }
+  });
+})();
